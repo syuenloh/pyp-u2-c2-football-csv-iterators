7ae911a93ca41faaf738c61127bf131c0ba10e27
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-25 18:33:02 +0800

    2nd attempt

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -8,10 +8,10 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        with open(self.csv_file_name, newline='') as csvfile:
-            player = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        with open(r'C:\Users\kaisyuenl\Data science projects\pyp-u2-c2-football-csv-iterators\test_data.csv') as csvfile:
+            player = csv.reader(csvfile, delimiter=',', quotechar='|')
             for row in player:
-                return Player(row)
+                return Player(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
 
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        return Player(self,number,position,name,date_of_birth,caps,club,country,club_country,year)
