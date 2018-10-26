107c381dd7b5b31a3d694a2c3efec4a218cdb023
Syuen Loh <syuenloh.ds@gmail.com>
2018-10-26 17:19:04 +0800

    fixed path to dataset

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -7,12 +7,12 @@ class FootballExplorer():
         self.csv_file_name = csv_file_name
 
     def all(self):
-        with open('/Users/kaisyuenloh/WWcodeKL/pyp-u2-c2-football-csv-iterators/test_data.csv') as csvfile:
+        with open('test_data.csv') as csvfile:
             player = csv.reader(csvfile, delimiter=',', quotechar='|')
             return [Player(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8]) for row in player]
 
     def search(self, country=None, year=None, age=None, position=None):
-        with open('/Users/kaisyuenloh/WWcodeKL/pyp-u2-c2-football-csv-iterators/test_data.csv') as csvfile:
+        with open('test_data.csv') as csvfile:
                 player = csv.reader(csvfile, delimiter=',', quotechar='|')
                 player_lst=[]
                 for row in player:
